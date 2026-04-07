166400f954feab70723fe8bc59ac8374798cda33
Gunveer Vilkhu <gunveerv123@gmail.com>
2026-04-06 21:46:21 -0400

    reading playlist file

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ a simple gui to manage ipod classic playlists, built with cpp
 * check for music and playlist folders
 * select a .m3u8 file and add/remove songs from music folder
 
+## Precompiled
+* a precompiled exe for silicon-based mac devices is added `mp3.out`
+
 ## engine future updates
 * be able to select music and playlist folders
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -9,6 +10,23 @@ std::string error(std::string message)
     std::cout << message << std::endl;
 
     return "";
+}
+
+void readPlaylist(std::string pathToPlaylist)
+{
+    std::string playlistText;
+    std::ifstream myReadFile(pathToPlaylist);
+
+    std::cout << "Reading playlist: " + pathToPlaylist << std::endl;
+
+    while(getline(myReadFile, playlistText))
+    {
+        std::cout << playlistText << std::endl;
+    }
+
+    std::cout << playlistText << std::endl;
+
+    myReadFile.close();
 }
 
 int main(int argc, char *argv[])
@@ -41,6 +59,10 @@ int main(int argc, char *argv[])
         std::string msg = "Error Path " + playerPath + "/Playlists is empty";
         error(msg);
     }
+
+    // Read Arugments
+    readPlaylist(playerPath + "/Playlists/AAA.m3u8");
+
 
     // loops through entire dir
     // for (const auto & entry : fs::directory_iterator(playerPath))
